30a87afdbf1c6c6957896faa7d449809a28756bb
Geoffry Millner <geoff.millner22@gmail.com>
2022-02-17 11:05:22 -0800

    add api log and event prevent def

--- src/exchange.js
+++ src/exchange.js
@@ -1,17 +1,29 @@
+// var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;// change this later
 export default class CurrencyExchange {
-  static getExchange(usd) {
+  static getExchange() {
+    console.log("api before promise");
     return new Promise(function (resolve, reject) {
+      console.log("api starting log in promise");
       let request = new XMLHttpRequest();
-      const url = `https://v6.exchangerate-api.com/v6/appid=${process.env.API_KEY}/latest/USD`;
+      const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`;
       request.onload = function () {
+        console.log(this);
         if (this.status === 200) {
           resolve(request.response);
+          console.log("hello");
         } else {
           reject(request.response)
         }
       }
-      request.open("GET, url, true");
+      request.open("GET", url, true);
       request.send();
     });
   }
 }
+
+// let test = CurrencyExchange.getExchange();
+// test.then(function (response) {
+//   console.log(response);
+// }, function (error) {
+//   console.log(error);
+// });
--- src/index.js
+++ src/index.js
@@ -2,12 +2,22 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import CurrencyExchange from './exchange.js'
 
 async function /*replace a with api call name here*/a() {
 }
 
 $(document).ready(function () {
-  $('').click(function () {
+  $('#userSelectionForm').submit(function (event) {
+    event.preventDefault();
+    let test = CurrencyExchange.getExchange();
+    test.then(function (response) {
+      console.log("inside then");
+      console.log(response);
+      console.log(test);
+    }, function (error) {
+      console.log(error);
+    });
   });
 });
 

0e25fec5ac43c07b32ec73c01219c4027ad0d93c
Geoffry Millner <geoff.millner22@gmail.com>
2022-02-25 16:02:28 -0800

    add function to output iso codes to html using jquery

--- src/index.js
+++ src/index.js
@@ -8,12 +8,21 @@ function math(firstNum, secondNum) {
   return firstNum * secondNum;
 }
 
+function outputIsoCodes(object) {
+  $("#isoApiCodes").html("");
+  let isoObjectKeys = Object.keys(object)
+  isoObjectKeys.forEach(isocode => {
+    $("#isoApiCodes").append(`<li>${isocode}</li>`)
+  })
+}
+
 $(document).ready(function () {
   $('#userSelectionForm').submit(function (event) {
     event.preventDefault();
     let test = CurrencyExchange.getExchange();
     test.then(function (response) {
       const body = JSON.parse(response);
+      outputIsoCodes(body["conversion_rates"]);
       console.log(body["conversion_rates"]);
       const userUsdSelection = $("input[name='usdTotal']").val();
       const isoCode = $("input[name='isoSelect']").val().toUpperCase();
@@ -28,6 +37,7 @@ $(document).ready(function () {
       } else {
         $('#isoNumberAmount').text(convertedCurrencyNumber);
       }
+      // add new click function or event listener on html to show iso codes/ fix toggle below
       $("#useableIso").toggle();
       $("#isoIdHtmlText").toggle();
     }, function (error) {
